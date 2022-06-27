53f0eeced048445450635fb4e57007963897a8fb
Avidu <avidu97@gmail.com>
2022-06-27 08:00:58 +0530

    Fragment Wrapper Added

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,10 +1,13 @@
 import Script from "next/script";
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-31W943248F"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
 
-    gtag('config', 'G-31W943248F');
-</script>
+<>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-31W943248F"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-31W943248F');
+    </script>
+</>
