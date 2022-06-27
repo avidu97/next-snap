6f5244d285ce7987ead4e1966bb24aeb69ddd835
Avidu <avidu97@gmail.com>
2022-06-27 09:15:28 +0530

    GAnalitics added correctly

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,22 @@
 import { SnipcartProvider } from "use-snipcart";
 import "@styles/globals.scss";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-31W943248F"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-31W943248F');
+        `}
+      </Script>
       <SnipcartProvider>
         <Component {...pageProps} />
       </SnipcartProvider>
