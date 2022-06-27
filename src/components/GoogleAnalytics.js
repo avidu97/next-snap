5a4bb7218e1eb3da1b9c5897632fe65866250518
Avidu <avidu97@gmail.com>
2022-06-27 07:56:31 +0530

    Google analytics added

---
+++ src/components/GoogleAnalytics.js
@@ -0,0 +1,10 @@
+import Script from "next/script";
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-31W943248F"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-31W943248F');
+</script>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import { SnipcartProvider } from "use-snipcart";
 import "@styles/globals.scss";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SnipcartProvider>
-      <Component {...pageProps} />
-    </SnipcartProvider>
+    <>
+      <GoogleAnalytics />
+      <SnipcartProvider>
+        <Component {...pageProps} />
+      </SnipcartProvider>
+    </>
   );
 }
 
